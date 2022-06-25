91633164e0ffead6615ce6f0c2d7dc7dec863386
“Alexey“ <nvmqwer593@gmail.com>
2022-06-26 01:38:53 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let answer = '';
+  n = String(n);
+  for (let i = n.length - 1; i >= 0; i--) {
+    if (n[i] !== '-') answer += n[i];
+  }
+  return answer;
 }
